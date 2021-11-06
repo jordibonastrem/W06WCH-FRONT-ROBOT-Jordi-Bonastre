af75da465aa51a9b1a6a0482c92d758ff0ec3e7e
Jordi <jordi.bonastre.m@gmail.com>
2021-11-06 19:00:38 +0100

    aded robotReducer and rootReducer

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,6 +1,8 @@
 const actionTypes = {
   loadRobots: "LOAD_ROBOTS",
   deleteRobots: "DELETE_ROBOT",
-  createTask: "CREATE_ROBOT",
-  updateTask: "UPDATE_ROBOT",
+  createRobots: "CREATE_ROBOT",
+  updateRobots: "UPDATE_ROBOT",
 };
+
+export default actionTypes;
---
+++ src/redux/reducers/robotsReducer.js
@@ -0,0 +1,22 @@
+import actionTypes from "../actions/actionTypes";
+
+const robotReducer = (robots = [], action) => {
+  let newTaskState = robots;
+  switch (action.type) {
+    case actionTypes.loadRobots:
+      newTaskState = [...action.robots];
+      break;
+    case actionTypes.createRobots:
+      newTaskState = [...robots, action.task];
+      break;
+    case actionTypes.deleteTask:
+      newTaskState = robots.filter((robot) => robot.id !== action.id);
+      break;
+
+    default:
+    // throw new Error("action non existent.");
+  }
+  return newTaskState;
+};
+
+export default robotReducer;
