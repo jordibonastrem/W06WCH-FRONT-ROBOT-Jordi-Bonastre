3e18ab97b59b49c0bea79712aa3901589f22073f
Jordi <jordi.bonastre.m@gmail.com>
2021-11-07 01:58:44 +0100

    add create test and delete test

--- src/redux/reducers/robotsReducer.test.js
+++ src/redux/reducers/robotsReducer.test.js
@@ -11,6 +11,39 @@ describe("Given a robotsReducer reducer", () => {
         type: actionTypes.loadRobotsAction,
         robots: robotsList,
       };
+
+      const newList = robotReducer(initialRobots, action);
+
+      expect(newList).toEqual(robotsList);
+    });
+  });
+  describe("When it receives an empty robots list and a create action with three robots", () => {
+    test("Then it should return a new robot list including the new robot", () => {
+      const initialRobots = getRandomRobots(3);
+      const newRobot = getRandomRobot();
+      const action = {
+        type: actionTypes.createRobotAction,
+        robot: newRobot,
+      };
+
+      const newList = robotReducer(initialRobots, action);
+
+      expect(newList).toContainEqual(newRobot);
+    });
+  });
+
+  describe("When it receives a robots list and a delete action with an id", () => {
+    test("Then it should return a new robots list without the robot received", () => {
+      const initialRobots = getRandomRobots(3);
+      const robotToDelete = initialRobots[0];
+      const action = {
+        type: actionTypes.deleteRobotAction,
+        id: robotToDelete._id,
+      };
+
+      const newList = robotReducer(initialRobots, action);
+
+      expect(newList).not.toContainEqual(robotToDelete);
     });
   });
 });
