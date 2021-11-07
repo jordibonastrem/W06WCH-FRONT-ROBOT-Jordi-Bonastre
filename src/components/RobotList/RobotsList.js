56aa5f4950da4651736e6dc968afedc4cab275f2
Jordi <jordi.bonastre.m@gmail.com>
2021-11-07 18:18:45 +0100

    bugfixdateobject

--- src/components/RobotList/RobotsList.js
+++ src/components/RobotList/RobotsList.js
@@ -10,6 +10,7 @@ const RobotsList = ({ robots }) => {
       <ul>
         {robots.map((robot) => {
           return <Robot key={robot._id} robot={robot}></Robot>;
+          // console.log(robot._id, robot);
         })}
       </ul>
     </div>
